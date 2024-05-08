1e6678c38a013122c84c7934490b144b785170f6
faithnicoletti <faithalicia22@gmail.com>
2024-05-08 07:52:59 -0400

    forms changes

--- main_app/forms.py
+++ main_app/forms.py
@@ -2,11 +2,11 @@ from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm,UserCre
 from django.contrib.auth.models import User
 from django import forms
 
-class CustomSignUpForm(UserCreationForm):
+class CustomUserChangeForm(UserChangeForm):
     email = forms.EmailField(required=True)
     first_name = forms.CharField(max_length=250)
     last_name = forms.CharField(max_length=250)
-    username = forms.CharField(max_length=250)
+    username = forms.Charfield(max_length=250)
 
     class Meta:
         model = User
@@ -15,10 +15,20 @@ class CustomSignUpForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields.pop('password')
-
+        
         for field_name in self.fields:
             self.fields[field_name].widget.attrs['class'] = 'form-control text-white'
 
+# class CustomUserChangeForm(UserChangeForm):
+#     class Meta(UserChangeForm.Meta):
+#         fields = ('username', 'email', 'first_name', 'last_name')
+
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.fields.pop('password')
+
+#         for field_name in self.fields:
+#             self.fields[field_name].widget.attrs['class'] = 'form-control text-white'
 
 class CustomPasswordChangeForm(PasswordChangeForm):
     def __init__(self, *args, **kwargs):
